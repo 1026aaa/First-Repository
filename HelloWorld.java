db655e2e7c57a7a3101dc8f46ab04e2fd17e31e2
1026aaa <1026984928@qq.com>
2026-03-09 11:28:48 +0800

    添加 HelloWorld.java 程序，作者：祝俊棋

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * HelloWorld 程序
+ * 作者：祝俊棋
+ * 功能：输出 Hello World!
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        // 核心输出语句
+        System.out.println("Hello World!");
+        System.out.println("作者：祝俊棋");
+    }
+}
